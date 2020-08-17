27636493318ab8d255a72127129169c3b12aabd5
Markiyan Derevetskyi <markiyan07345@gmail.com>
2020-08-17 12:58:12 +0300

    styled student table

--- src/component/StudentRow.js
+++ src/component/StudentRow.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-import Header from "./Header";
 
 class StudentRow extends React.Component {
 
--- src/component/StudentTable.js
+++ src/component/StudentTable.js
@@ -1,6 +1,6 @@
 import * as React from "react";
-import Header from "./Header";
 import StudentRow from "./StudentRow";
+import "../css/studentTable.css";
 
 class StudentTable extends React.Component {
 
