f74990ac201dd03c1eee8b7b840bdbecd89bc7ef
Markiyan Derevetskyi <markiyan07345@gmail.com>
2020-08-21 20:40:30 +0300

    added delete student button and function that handles it and send call to server

--- src/component/StudentRow.js
+++ src/component/StudentRow.js
@@ -6,20 +6,27 @@ class StudentRow extends React.Component {
         super(props);
 
         this.handleTransferClick = this.handleTransferClick.bind(this);
+        this.handleDeleteClick = this.handleDeleteClick.bind(this);
     }
 
     async handleTransferClick(event) {
         event.preventDefault();
-
         const studentId = this.props.student.id;
         const groupName = prompt('Please, choose group');
 
         if (groupName == null) {
             return;
         }
-
         await fetch(`http://localhost:8080/students/${studentId}/transfer?groupName=${groupName}`, {
             method: 'GET'
+        });
+    }
+
+    async handleDeleteClick() {
+        const studentId = this.props.student.id;
+
+        await fetch(`http://localhost:8080/students/${studentId}`, {
+            method: 'DELETE',
         });
     }
 
@@ -34,6 +41,7 @@ class StudentRow extends React.Component {
                 <td>{student.lastName}</td>
                 <td>{student.age}</td>
                 <td><button onClick={this.handleTransferClick}>Transfer</button></td>
+                <td><button onClick={this.handleDeleteClick}>Delete</button></td>
             </tr>
         );
     }
